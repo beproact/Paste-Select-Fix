18160e7c85c0a2fabdae6d59eb230acaffc11ef6
JasonALow <jasonalow8@gmail.com>
2025-05-09 06:20:57 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+//remember to remove MacOS
+#include <Geode/modify/EditorUI.hpp>
+class $modify(MyEditorUI, EditorUI) {
+    void onDuplicate(CCObject* sender) {
+        createUndoSelectObject(true);
+        EditorUI::onDuplicate(sender);
+    }
+    void doPasteObjects(bool p0) {
+        if(getSelectedObjects()->count()){
+            createUndoSelectObject(true);
+        }
+        EditorUI::doPasteObjects(p0);
+    }
+
+};
